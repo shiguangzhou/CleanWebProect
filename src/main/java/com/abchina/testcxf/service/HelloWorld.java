6a98e074a4a1e28c52f39934c953881c37b81af8
shiguangzhou <shiguangzhou1989@gmail.com>
2017-05-14 10:55:17 +0800

    add some comment

--- src/main/java/com/abchina/testcxf/service/HelloWorld.java
+++ src/main/java/com/abchina/testcxf/service/HelloWorld.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Service;
 
 //@WebService(targetNamespace="http://impl.service.testcxf.abchina.com/")
 //@Service
-@WebService
+// 不要在接口中标记WebService注解，否则客户端不能够正常访问wsdl
+//@WebService
 public interface HelloWorld {
 //	@WebMethod
 	public String sayHello(String text);
