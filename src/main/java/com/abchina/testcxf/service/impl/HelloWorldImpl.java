0e615c613b106dd4e4cb74672c4c5fec62c177f9
shiguangzhou <shiguangzhou1989@gmail.com>
2017-05-21 01:28:23 +0800

    common-logging 使用jdk1.4 开始自带的log

--- src/main/java/com/abchina/testcxf/service/impl/HelloWorldImpl.java
+++ src/main/java/com/abchina/testcxf/service/impl/HelloWorldImpl.java
@@ -3,6 +3,8 @@ package com.abchina.testcxf.service.impl;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,13 @@ import com.abchina.testcxf.service.HelloWorld;
 @WebService
 @Component
 public class HelloWorldImpl implements HelloWorld {
-
+	private static final Log log = LogFactory.getLog(HelloWorldImpl.class);
+	
 	@WebMethod
 	public String sayHello(String text) {
+		log.info("log helloworld " + text + "\t" +  log.getClass());
 		return "helloworld" + text;
+		
 	}
 	
 	public HelloWorldImpl(){};
